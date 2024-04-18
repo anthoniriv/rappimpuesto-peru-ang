6f83f54c7bb7a04f5402d96db31a84872d4f97c7
MAURO <maurosalas153117@gmail.com>
2024-04-18 12:22:50 -0500

    changes

--- src/app/calculators/calc-precio/calc-precio.page.ts
+++ src/app/calculators/calc-precio/calc-precio.page.ts
@@ -6,10 +6,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calc-precio.page.scss'],
 })
 export class CalcPrecioPage implements OnInit {
+  compras: string = "";
+  gastos: string = "";
+  ganancia1: string = "";
+  igv: string = "";
+  precio: string = "";
+  ganancia2: string = "";
+  showPopover: boolean = false;
+  popoverContent: string = "";
 
-  constructor() { }
-
-  ngOnInit() {
+  handleInfoClick(content: any) {
+    this.popoverContent = content;
+    this.showPopover = true;
   }
 
+  handleChangeCompras(value: any) {
+    this.compras = value;
+    this.calcularIGV(value, this.gastos, this.ganancia1);
+  }
+
+  handleChangeGastos(value: any) {
+    this.gastos = value;
+    this.calcularIGV(this.compras, value, this.ganancia1);
+  }
+
+  handleChangeGanancia1(value: any) {
+    this.ganancia1 = value;
+    this.calcularIGV(this.compras, this.gastos, value);
+  }
+
+  calcularIGV(compras: string, gastos: string, ganancia1: string) {
+    if (
+      compras !== "" &&
+      compras !== undefined &&
+      gastos !== "" &&
+      gastos !== undefined &&
+      ganancia1 !== "" &&
+      ganancia1 !== undefined
+    ) {
+      const total =
+        parseFloat(compras) + parseFloat(gastos) + parseFloat(ganancia1);
+      const igvCalculado = total * 0.18;
+      this.igv = igvCalculado.toFixed(2);
+
+      const precioCalculado = total + igvCalculado;
+      this.precio = precioCalculado.toFixed(2);
+
+      const ganancia2Calculada = igvCalculado - parseFloat(this.igv);
+      this.ganancia2 = ganancia2Calculada.toFixed(2);
+    } else {
+      this.igv = "";
+      this.precio = "";
+      this.ganancia2 = "";
+    }
+  }
+
+  handleReset() {
+    this.compras = "";
+    this.gastos = "";
+    this.ganancia1 = "";
+    this.igv = "";
+    this.precio = "";
+    this.ganancia2 = "";
+  }
+
+  ngOnInit() {
+    // Initialization code here
+    console.log("CalcPrecioPage initialized");
+  }
 }
